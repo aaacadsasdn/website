019d106cc25fd0596b01dcd9436f7ea3279ecd4f
aaacadsasdn <aaacadsasdn10071993@gmail.com>
2020-12-17 15:32:34 +0900

    add

--- Django/settings.py
+++ Django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '-3c(dk!l1p&yf$x%73kpa#grqsbgnbq_#khm**la5+dho7b9&='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
